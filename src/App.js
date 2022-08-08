8e20edf313cbcb30d4db450afd3faf84c2e636cb
thyoondev <th.yoon.dev@gmail.com>
2022-08-08 15:49:27 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+  //web3 연동
+  const [web3, setWeb3] = useState();
+  const [account, setAccount] = useState('');
+
+  useEffect(() => {
+    if (typeof window.ethereum !== 'undefined') {
+      // window.ethereum이 있다면
+      try {
+        const web = new Web3(window.ethereum); // 새로운 web3 객체를 만든다
+        setWeb3(web);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
+
+  //지갑 연동
+  const connectWallet = async () => {
+    let accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+
+    setAccount(accounts[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*상태확인*/}
+      {console.log(web3)}
+      {console.log('account : ' + account)}
+
+      {/*지갑 버튼 */}
+      <button
+        className="metaConnect"
+        onClick={() => {
+          connectWallet();
+        }}
+      >
+        connect to MetaMask
+      </button>
     </div>
   );
 }
