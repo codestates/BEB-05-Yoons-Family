dcc6eacf82a1c43a1294074b2589712092c8719e
thyoondev <th.yoon.dev@gmail.com>
2022-08-11 06:49:57 +0900

    add footerdata

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,22 +1,39 @@
-import { Avatar, Comment as CommentAntd } from 'antd';
-import React, { useState } from 'react';
+import { Avatar, Comment as _Comment } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
 import { theme } from '../style/theme';
 
-const Comment = () => {
+const Comment = ({ footerData }) => {
   return (
-    <CommentAntd
-      style={{ color: `${theme.white}` }}
-      author={<a style={{ color: `${theme.white}` }}>Yoon XX</a>}
-      avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Yoon XX" />}
+    <CommentWrapper
+      author={
+        <p
+          style={{
+            color: `${theme.soft_blue}`,
+            fontSize: `${theme.fs_3}`,
+          }}
+        >
+          {footerData.github_id}
+        </p>
+      }
+      avatar={<Avatar src={footerData.avatar} alt={footerData.github_id} />}
       content={
-        <p>
+        <div>
           It's fun and exciting to do a project based on what we learned. It's
           fun and exciting to do a project based on what we learned. It's fun
           and exciting to do a project based on what we learned.
-        </p>
+        </div>
       }
     />
   );
 };
+
+const CommentWrapper = styled(_Comment)`
+  color: ${theme.very_dark_blue_line};
+
+  :hover {
+    color: ${theme.soft_blue};
+  }
+`;
 
 export default Comment;
